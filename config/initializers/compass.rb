c0ec6c2e361ee19d351c582221f8ab18816e246a
Dinshaw Gobhai <gobhai@gmail.com>
2011-09-13 12:13:20 -0400

    add compass config

---
+++ config/initializers/compass.rb
@@ -0,0 +1,11 @@
+project_type = :rails
+http_path    = '/'
+css_dir      = 'tmp/stylesheets'
+sass_dir     = 'app/views/stylesheets'
+
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
