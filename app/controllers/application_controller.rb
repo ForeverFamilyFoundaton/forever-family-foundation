3318621c0c38416085b6ab5337f185ab973ba532
Dinshaw <gobhai@gmail.com>
2012-02-20 18:59:28 -0500

    remove http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   before_filter :get_cms_page
-  before_filter :http_auth unless ['development','test'].include?(Rails.env)
   before_filter :adg_redirect
   
   def http_auth
