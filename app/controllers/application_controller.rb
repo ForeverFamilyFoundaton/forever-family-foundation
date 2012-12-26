6f175264e84d397ca482d25fd58515536f257e0d
Dinshaw Gobhai <gobhai@gmail.com>
2012-12-26 17:06:00 -0400

    Remove acts_as_list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   before_filter :get_cms_page
   # before_filter :adg_redirect
-  
+
   def http_auth
     if !session[:authenticated]
       authenticate_or_request_with_http_basic do |username, password|
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
 private
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
@@ -23,7 +23,7 @@ private
     controller = self.controller_name.titleize
     # Admin user should not need to differentiate between edit / update and new / create
     action = case self.action_name
-    when 'create' 
+    when 'create'
       'New'
     when 'update'
       'Edit'
@@ -32,12 +32,12 @@ private
     end
     [controller,action, params[:step]].compact.join(": ")
   end
-  
+
   def get_cms_page
     return if request.fullpath =~ /admin/
     @cms_page ||=  if current_reference_string == 'Site: Page'
-      CmsPage.find(params[:id])  
-    else    
+      CmsPage.find(params[:id])
+    else
       CmsPage.get(current_reference_string)
     end
   end
@@ -45,15 +45,15 @@ private
   def store_location
     session[:return_to] = request.fullpath
   end
-  
+
   # def welcome
   #   if current_user.try :needs_to_be_welcomed?
   #     url = current_user.biz? ? business_wecome_path(current_user) :  users_wecome_path(current_user)
   #     redirect_to url
   #   end
   # end
-  
-#   def adg_redirect 
+
+#   def adg_redirect
 #     if session[:adg_registration] && current_user && current_user.welcomed?
 #       redirect_to new_adg_registration_path
 #     end
