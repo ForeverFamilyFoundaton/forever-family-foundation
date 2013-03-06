65b27a040849e6f671d5c01f68c0347ef8c8d814
Dinshaw Gobhai <gobhai@gmail.com>
2013-03-05 21:17:06 -0500

    Add user mailer templateos

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,12 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user, welcome_email)
     @user = user
     # @url  = "http://example.com/login"
-    body = welcome_email.body
+    @body = welcome_email.body
     subject = welcome_email.subject
     user.attributes.keys.each do |key|
-      body.gsub!("@#{key.to_s}", user[key].to_s)
+      @body.gsub!("@#{key.to_s}", user[key].to_s)
       subject.gsub!("@#{key.to_s}", user[key].to_s)
     end
-    mail(to: user.email, subject: subject, body: body, content_type: "text/html")
+    mail(to: user.email, subject: subject)
   end
 end
