a77fc9896892082308acc2d48557e484633bf82f
Dinshaw Gobhai <gobhai@gmail.com>
2013-05-22 22:31:23 -0400

    add gmail settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'foreverfamilyfoundation.org',
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
