86c1597df154289e11852cb4052f6d897b100692
Dinshaw Gobhai <gobhai@gmail.com>
2013-05-22 22:38:36 -0400

    add gmail settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,5 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'foreverfamilyfoundation.org',
