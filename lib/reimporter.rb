33b97d791f4926685ec505c005b0197e400a34c8
Dinshaw <gobhai@gmail.com>
2014-06-20 17:05:28 -0400

    woking on it

--- lib/reimporter.rb
+++ lib/reimporter.rb
@@ -3,39 +3,48 @@ require 'spreadsheet'
 class Reimporter
   def self.process(file)
     xls = Roo::Spreadsheet.open file
-    xls.each_with_index(MAPPING) do |attrs, i|
-      next if i == 0
-      user_attrs = []
-      user_attrs <<  {
-        password: password = SecureRandom.hex(8),
-        password_confirmation: password,
-        membership_number: attrs[:membership_number] || attrs[:id],
-        first_name: attrs[:first_name],
-        middle_name: attrs[:middle_name],
-        middle_name: attrs[:middle_name],
-        last_name: attrs[:last_name],
-        email: attrs[:email],
-        email: attrs[:email],
-        cell_phone: attrs[:cell_phone],
-        home_phone: attrs[:home_phone],
-        work_phone: attrs[:work_phone],
-        work_phone: attrs[:work_phone],
-        fax: attrs[:fax],
-        is_business: attrs[:is_business],
-        address_attributes: {
-          address: attrs[:address_street],
-          city: attrs[:address_city],
-          state: attrs[:address_state],
-          zip: attrs[:address_zip],
-          country: attrs[:address_country],
-        },
-        enrolled_from: attrs[:enrolled_from],
-        do_not_mail: attrs[:do_not_mail],
-        problems: attrs[:problems],
-      }
-      User.transaction do
-        Rails.logger.info("Creating #{user_attrs.size} users..." )
-        User.create user_attrs
+    i = 0
+
+    xls.each_with_pagename do |_, sheet|
+      sheet.each(MAPPING) do |attrs|
+        next if attrs[:id] == 'Id'
+
+        user_attrs = []
+        user_attrs <<  {
+          password: password = SecureRandom.hex(8),
+          password_confirmation: password,
+          membership_number: attrs[:membership_number] || attrs[:id],
+          first_name: attrs[:first_name],
+          middle_name: attrs[:middle_name],
+          middle_name: attrs[:middle_name],
+          last_name: attrs[:last_name],
+          email: attrs[:email],
+          email: attrs[:email],
+          cell_phone: attrs[:cell_phone],
+          home_phone: attrs[:home_phone],
+          work_phone: attrs[:work_phone],
+          work_phone: attrs[:work_phone],
+          fax: attrs[:fax],
+          is_business: attrs[:is_business],
+          address_attributes: {
+            address: attrs[:address_street],
+            city: attrs[:address_city],
+            state: attrs[:address_state],
+            zip: attrs[:address_zip],
+            country: attrs[:address_country],
+          },
+          enrolled_from: attrs[:enrolled_from],
+          do_not_mail: attrs[:do_not_mail],
+          problems: attrs[:problems],
+        }
+        ActiveRecord::Base.transaction do
+          user_attrs.each do |attrs|
+            puts 'Creating ...'
+            u = User.new
+            u.assign_attributes(attrs)
+            u.save!
+          end
+        end
       end
     end
   end
