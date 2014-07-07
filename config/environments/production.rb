61298e715486f7c825bc32f8b10c282a33d730db
Dinshaw <gobhai@gmail.com>
2014-07-07 12:39:44 -0400

    Fix prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,5 @@ ForeverFamilyFoundation::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { host: 'foreverfamilyfoundation.org' }
-}
 
 end
