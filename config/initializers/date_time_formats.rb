f2b0bc0dc6e4a524936be47da2d44bf3d3cefbeb
Isaac Rosenberg <irosenb7@gmail.com>
2014-09-09 15:16:18 -0700

    Fixed date display
    fix #5

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -2,5 +2,6 @@
 Date::DATE_FORMATS[:events] = '%m/%d/%Y'
 Time::DATE_FORMATS[:admin] = '%m/%d/%y %H:%M'
 Time::DATE_FORMATS.merge! :events => lambda { |time|
-  time.strftime("%b #{ActiveSupport::Inflector.ordinalize(time.day)},  %l:%M %p %Y")
+  time.strftime("%b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y %l:%M %p")
 }
+Time::DATE_FORMATS[:ordinal_date] = -> (time) { time.strftime("%b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y")}
