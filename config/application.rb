1d4efed940fe65a26299699014e3da6023d0cf24
Dinshaw <gobhai@gmail.com>
2014-09-11 14:54:49 -0400

    bump asset version for heroku

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module ForeverFamilyFoundation
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     config.action_view.field_error_proc = Proc.new {
       |html_tag, instance| "<span class='field_with_errors'>#{html_tag}</span>".html_safe
