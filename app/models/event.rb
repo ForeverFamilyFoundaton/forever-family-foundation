969a946731cb5b874128d631095676bb0d0647c4
Dinshaw <gobhai@gmail.com>
2014-09-16 11:17:54 -0400

    Attrs to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,8 @@ class Event < ActiveRecord::Base
   scope :upcoming, -> { where("start_time >= ?", Time.now).order('start_time asc').limit(4) }
   scope :all_upcoming, -> { where("start_time >= ?", Time.now).order('start_time asc') }
 
+  attr_accessible :title, :url, :description, :start_time, :end_time
+
   def times
     if start_time && end_time
       [start_time.to_s(:events), end_time.to_s(:events)].join(' to ')
