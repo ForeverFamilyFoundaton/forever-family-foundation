6047c06415968d7cd9b90b711999843155df3343
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-13 10:59:47 -0700

    switch over

---
+++ app/helpers/announcements_helper.rb
@@ -0,0 +1,9 @@
+module AnnouncementsHelper
+  def announcement_hidden?(announcement)
+    cookies["announcement_#{announcement.created_at}"] == "hidden"
+  end
+
+  def current_announcement
+    @current_announcement ||= Announcement.current
+  end
+end
