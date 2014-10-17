c206785323b5ba2987dfc45c33de64f1d6415425
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-17 14:01:37 -0700

    Added specs

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,17 +1,27 @@
 require 'spec_helper'
 
 describe Announcement do
-  before { FactoryGirl.create(:announcement) }
-
+  subject { FactoryGirl.build(:announcement) } 
   it { should validate_uniqueness_of :start_date }
   it { should validate_uniqueness_of :end_date }
   it { should validate_presence_of :button }
+  it { should validate_presence_of :body }
   it { should validate_presence_of :link }
 
   context 'when two announcements overlap' do
     it "should not save" do
+      FactoryGirl.create(:announcement)
       announcement = FactoryGirl.build(:announcement)
       expect(announcement.save).to eq(false) 
+    end
+  end
+
+  context 'end date earlier than start date' do
+    it "will not save" do
+      subject { FactoryGirl.build(:announcement) }
+      subject.end_date = subject.start_date - 1.day
+      expect(subject.save).to eq(false)
+      # subject {  } 
     end
   end
 
