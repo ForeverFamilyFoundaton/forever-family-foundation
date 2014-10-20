a1d8d5a4242e3476bcf12938f3e2f152bc70a746
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-20 13:53:12 -0700

    end before start

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,6 +2,7 @@ class Announcement < ActiveRecord::Base
   validates_presence_of :button, :link, :body, :start_date, :end_date
   validates_uniqueness_of :start_date, :end_date
   validate :overlapping_start_dates, :overlapping_end_dates
+  validate :overlapping_start_dates, :overlapping_end_dates, :end_before_start
 
   def date_range
     start_date..end_date
@@ -15,6 +16,10 @@ class Announcement < ActiveRecord::Base
   def overlapping_end_dates 
     errors.add(:end_date, "cannot overlap with another announcement") unless self.class.where(end_date: start_date..end_date).empty?
   end 
+
+  def end_before_start
+    errors.add(:end_date, "cannot be earlier than start date") if start_date > end_date
+  end
   
   def self.current
     where("end_date >= ?", Date.today).first
