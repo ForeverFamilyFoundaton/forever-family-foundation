c30bbd8cfbd365c215a8c38c14b9351a558941f4
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-23 15:53:18 -0700

    better validation

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,21 +1,25 @@
 class Announcement < ActiveRecord::Base
   validates_presence_of :button, :link, :body, :start_date, :end_date
   validates_uniqueness_of :start_date, :end_date
-  validate :overlapping_start_dates, :overlapping_end_dates
-  validate :overlapping_start_dates, :overlapping_end_dates, :end_before_start
+  validate :end_before_start, :overlapping_dates
 
   def date_range
     start_date..end_date
   end
 
   def overlapping_start_dates
-    # binding.pry
+    self.class.where("start_date <= :start_date", :start_date)
     errors.add(:start_date, "cannot overlap with another announcement") unless self.class.where(start_date: start_date..end_date).empty?
   end
   
   def overlapping_end_dates 
     errors.add(:end_date, "cannot overlap with another announcement") unless self.class.where(end_date: start_date..end_date).empty?
   end 
+
+  def overlapping_dates
+    announcements = Announcement.all
+    errors.add(:base, "Announcement conflicts with other announcements") if announcements.any? { |announcement| announcement.date_range.overlaps? date_range }
+  end
 
   def end_before_start
     errors.add(:end_date, "cannot be earlier than start date") if start_date > end_date
