46e60d9aa9397782eb20d56d332b6505747b9c0a
peterpih <ppih@panix.com>
2015-09-18 00:14:50 +0100

    Gemfile	updated: fog, honeybadger, newrelic_rpm to get rid of duplicate key 	messages
    announcments_helper.rb - check for current date between start_date and end_date
    announcment.rb - use define? and change @current_announcement to curr_announcement
    
    Announcement testing
    
    Announcement testing, create and overlapping dates
    
    announcement testing
    
    Announcement specs
    
    Squash(4) of commits for Announcement checking of overlapping dates
    and indentation adjustments, includes scope of .current record for announcement
    
    Commit for spec files supporting refactoring of Announcement
    
    Commit for models files supporting Annoucement Update
    
    Commit for Announcement helper file and Gemfiles
    
    Changed tab-stop on models/announcement.rb and spec/factories/announcement.rb
    
    changed indent to spaces app/models/address.rb
    
    convert indent to spaces app/helpers/announcements_helper.rb
    
    removed Gemfile.lock from .gitignore
    
    removed :id from attr_acessible line
    
    indentation for spec/factories/announcements.rb
    
    merge conflicts resolved
    
    scope with logger.debugs
    
    commit for scope announcement.current
    
    delete doc/Annoucements.md
    
    refactor testing for current announcements

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
+  factory :announcement_empty do
+
+  end
   factory :announcement do
-    sequence(:start_date) { |n| Date.today + n.days }
-    sequence(:end_date) { |n| start_date + n.weeks }
+    start_date { Date.today }
+    end_date { Date.today + 1.weeks }
+
     body "Conference this week"
     button "Sign up"
     link "http://google.com"
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,28 +1,48 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Announcement do
-  subject { FactoryGirl.build(:announcement) }
-  
-  it { should validate_presence_of :button }
-  it { should validate_presence_of :body }
-  it { should validate_presence_of :link }
+  let(:announcement) { create(:announcement, start_date: 1.day.from_now, end_date: 5.days.from_now) }
+  let(:announcement_2) { build(:announcement, start_date: 5.days.from_now, body: "another test") }
 
-  context 'when two announcements overlap' do
-    it "should not save" do
-      FactoryGirl.create(:announcement)
-      announcement = FactoryGirl.build(:announcement)
-      expect(announcement.save).to eq(false)
+  it 'is invalid without #button' do
+    announcement.button = nil
+    expect(announcement).not_to be_valid
+  end
+
+  it 'is invalid without #body' do
+    announcement.body = nil
+    expect(announcement).not_to be_valid
+  end
+
+  it 'is invalid without #link' do
+    announcement.link = nil
+    expect(announcement).not_to be_valid
+  end
+
+  it 'is invalid without #start_date' do
+    announcement.start_date = nil
+    expect(announcement).not_to be_valid
+  end
+
+  it 'is invalid without #end_date' do
+    announcement.end_date = nil
+    expect(announcement).not_to be_valid
+  end
+
+  context 'validate overlapping dates' do
+    before do
+      announcement
+      announcement_2
+    end
+    it 'is invalid with overlapping dates' do
+      expect(announcement_2).not_to be_valid
     end
   end
 
-  context 'end date earlier than start date' do
-    it "will not save" do
-      subject { FactoryGirl.build(:announcement) }
-      subject.end_date = subject.start_date - 1.day
-      expect(subject.save).to eq(false)
+  context 'with end_date < start_date' do
+    it 'is invalid' do
+      announcement.end_date = announcement.start_date - 1.day
+      expect(announcement).not_to be_valid
     end
   end
-
-  after { Announcement.destroy_all }
-
 end
