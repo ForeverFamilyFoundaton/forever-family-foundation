9518da15168dc805cf18fe26c6076b8aab6d45f2
peterpih <ppih@panix.com>
2016-04-12 08:48:20 -0500

    show ID and Content-Type in Admin for Attach Files

--- app/admin/attached_files.rb
+++ app/admin/attached_files.rb
@@ -1,11 +1,17 @@
 ActiveAdmin.register AttachedFile do
   menu false
   index do
+    column 'ID' do |q|
+      q.id.to_s
+    end
     column 'Thumbnail (if image)' do |q|
       image_tag q.attachment.url(:thumb) if q.image?
     end
     column 'File name/path' do |q|
       link_to q.attachment_file_name, q.attachment.url
+    end
+    column 'Type' do |q|
+      q.attachment_content_type
     end
     column "Actions" do |q|
       link_to "Delete", admin_attached_file_path(q, method: :delete)
