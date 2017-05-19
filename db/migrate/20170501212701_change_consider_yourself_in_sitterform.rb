9c725bfbfcada43952bc51cd0d7fd02c1143018f
railsnewbie257 <railsnewbie257@gmail.com>
2017-05-19 16:35:21 -0500

    delete a migration

--- db/migrate/20170501212701_change_consider_yourself_in_sitterform.rb
+++
@@ -1,8 +0,0 @@
-class ChangeConsiderYourselfInSitterform < ActiveRecord::Migration
-  def up
-    change_column :sitterforms, :consider_yourself, :integer
-  end
-  def down
-    change_column :sitterforms, :consider_yourself, :integer
-  end
-end
