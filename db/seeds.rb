7b37ffadb969b15eb8d1d35c5fd62a776d72ecf7
railsnewbie257 <railsnewbie257@gmail.com>
2017-05-24 20:49:01 -0500

    seeds.rb for BeliefType Relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
+BeliefType.destroy_all
+BeliefType.create!([  {name: "A total non-believer"}, ])
+BeliefType.create!([  {name: "Accepting and open to the possibility"}, ])
+BeliefType.create!([  {name: "A firm believer "}, ])
 
-Relationship.create!([  {name: "Mother", typeof: "family"}, ])
+Relationship.destroy_all
+Relationship.create!([  {name: "Choose One", typeof: "none"}, ])
 Relationship.create!([  {name: "Father", typeof: "family"}, ])
+Relationship.create!([  {name: "Mother", typeof: "family"}, ])
 Relationship.create!([  {name: "Sister", typeof: "family"}, ])
 Relationship.create!([  {name: "Brother", typeof: "family"}, ])
 Relationship.create!([  {name: "Uncle", typeof: "family"}, ])
@@ -9,5 +15,5 @@ Relationship.create!([  {name: "Husband", typeof: "family"}, ])
 Relationship.create!([  {name: "Wife", typeof: "family"}, ])
 Relationship.create!([  {name: "Son", typeof: "family"}, ])
 Relationship.create!([  {name: "Daughter", typeof: "family"}, ])
+Relationship.create!([  {name: "Cousin", typeof: "family"}, ])
 Relationship.create!([  {name: "Other", typeof: "family"} ])
-Relationship.create!([  {name: "Someone", typeof: "family"} ])
