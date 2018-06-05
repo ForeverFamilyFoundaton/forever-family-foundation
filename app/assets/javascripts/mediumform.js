34f6bab6f50854314339a4f0e68d87eed4f00b9d
railsnewbie257 <railsnewbie257@gmail.com>
2018-06-05 17:57:16 -0500

    fixed description for professional fields

--- app/assets/javascripts/mediumform.js
+++ app/assets/javascripts/mediumform.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 tom();
-  document.getElementById('mediumform_personalprofessional_use_fff').onclick  = function() 
+  document.getElementById('mediumform_personalprofessional_fff_is_professional').onclick  = function() 
   {document.getElementById('alternateinfo').innerHTML = "Professional Information";}
-    document.getElementById('mediumform_personalprofessional_use_alternate').onclick  = function() 
+    document.getElementById('mediumform_personalprofessional_alternate_is_professional').onclick  = function() 
   {document.getElementById('alternateinfo').innerHTML = "Personal Information";}
   
 });
