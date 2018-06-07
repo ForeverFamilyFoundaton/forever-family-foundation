d8e7fcf2689cb69f3b5375ac8152066eb5e77d7b
railsnewbie257 <railsnewbie257@gmail.com>
2018-06-07 16:23:42 -0500

    personalprofessional js fix

--- app/assets/javascripts/mediumform.js
+++ app/assets/javascripts/mediumform.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 tom();
   document.getElementById('mediumform_personalprofessional_fff_is_professional').onclick  = function() 
-  {document.getElementById('alternateinfo').innerHTML = "Professional Information";}
-    document.getElementById('mediumform_personalprofessional_alternate_is_professional').onclick  = function() 
   {document.getElementById('alternateinfo').innerHTML = "Personal Information";}
+    document.getElementById('mediumform_personalprofessional_alternate_is_professional').onclick  = function() 
+  {document.getElementById('alternateinfo').innerHTML = "Professional Information";}
   
 });
 
