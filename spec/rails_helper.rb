6596e57d2fbabef7664f51f1258cbd00f0257650
Dinshaw <gobhai@gmai.com>
2020-01-22 12:23:20 -0700

    Remove redundant inclusion of spec/support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,8 +32,6 @@ module ControllerMacros
   end
 end
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
