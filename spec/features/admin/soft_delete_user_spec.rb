dd8acaaee8f11d43597b353163c1245133e39e89
Dinshaw <gobhai@gmai.com>
2020-02-06 15:43:59 -0700

    Soft-delete User * Add discard gem * Create scopes and actions in admin area
    Devise is still checking emails against User.all, so a discarded email will still
    trigger the "Email is already taken" error.

---
+++ spec/features/admin/soft_delete_user_spec.rb
@@ -0,0 +1,26 @@
+RSpec.feature 'As an admin user' do
+  let!(:user) { create(:user) }
+
+  before do
+    sign_in_as_admin
+  end
+
+  scenario 'I can soft delete a user', :js do
+    visit admin_users_path
+    within "#user_#{user.id}" do
+      click_on user.email
+    end
+    accept_confirm do
+      click_on 'Soft-Delete'
+    end
+    expect(page).not_to have_content user.email
+    click_on 'Soft Deleted'
+    within "#user_#{user.id}" do
+      click_on user.email
+    end
+    accept_confirm do
+      click_on 'Un-Soft-Delete'
+    end
+    expect(page).to have_content user.email
+  end
+end
