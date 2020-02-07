b616c410218eb7b8e3741bb242560178d01144a6
Dinshaw <gobhai@gmai.com>
2020-02-07 09:59:19 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,2 @@
-BeliefType.destroy_all
-BeliefType.create!([  {name: "A total non-believer"}, ])
-BeliefType.create!([  {name: "Accepting and open to the possibility"}, ])
-BeliefType.create!([  {name: "A firm believer "}, ])
-
-Relationship.destroy_all
-Relationship.create!([  {name: "Choose One", typeof: "none"}, ])
-Relationship.create!([  {name: "Father", typeof: "family"}, ])
-Relationship.create!([  {name: "Mother", typeof: "family"}, ])
-Relationship.create!([  {name: "Sister", typeof: "family"}, ])
-Relationship.create!([  {name: "Brother", typeof: "family"}, ])
-Relationship.create!([  {name: "Uncle", typeof: "family"}, ])
-Relationship.create!([  {name: "Aunt", typeof: "family"}, ])
-Relationship.create!([  {name: "Husband", typeof: "family"}, ])
-Relationship.create!([  {name: "Wife", typeof: "family"}, ])
-Relationship.create!([  {name: "Son", typeof: "family"}, ])
-Relationship.create!([  {name: "Daughter", typeof: "family"}, ])
-Relationship.create!([  {name: "Cousin", typeof: "family"}, ])
-Relationship.create!([  {name: "Other", typeof: "family"} ])
+AdminUser.create! email: 'admin@example.com', password: 'password'
+CmsPage.create! reference_string: 'Site: Index', title: 'First page'
