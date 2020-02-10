deb2a48a4a92bfcbf309cd1b9af1defd7aed7f4c
Dinshaw <gobhai@gmai.com>
2020-02-09 19:25:54 -0700

    Try and get sidekiq web working

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,3 @@
-schedule_file = Rails.root.join('config/schedule.yml')
-
-if File.exist?(schedule_file)
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
-
 if Rails.env.development?
   require 'sidekiq/testing'
   Sidekiq::Testing.inline!
